282129eaa62157705d679e5b01fe225447a3a644
0n0ng <hy991006@gmail.com>
2025-11-06 15:20:47 +0900

    controller work

--- src/main/java/com/ll/chatApp/domain/chat/chatRoom/controller/ApiChatRoomController.java
+++ src/main/java/com/ll/chatApp/domain/chat/chatRoom/controller/ApiChatRoomController.java
@@ -1,7 +1,25 @@
 package com.ll.chatApp.domain.chat.chatRoom.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiChatRoomController {
+
+    @GetMapping("/api/v1/chat/rooms")
+    public String getChatRooms () {
+        return "채팅방 목록 조회 완료";
+    }
+
+    @GetMapping("/api/v1/chat/rooms/{roomId}")
+    public String getChatRoom (@PathVariable("roomId") Long roomId) {
+        return roomId + "번 채팅방 조회 완료";
+    }
+
+    @PostMapping("/api/v1/chat/rooms/{roomId}")
+    public String createChatRooms (@PathVariable("roomId") Long roomId) {
+        return "채팅방 생성 완료";
+    }
 }
